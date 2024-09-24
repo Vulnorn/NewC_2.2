be4759d49a50076ce31e3a60c87765b101a15930
Vulnorn <mixadrbk31@gmail.com>
2024-09-24 10:02:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NewC_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            string messageExite = "exite";
+
+            Console.WriteLine($"Введите сообщение: ");
+            message = Console.ReadLine();
+
+            while (message != messageExite)
+            { 
+                Console.WriteLine($"Вы ввели: {message}; Для выхода введите - {messageExite}");
+                Console.WriteLine($"Введите сообщение: "); 
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
